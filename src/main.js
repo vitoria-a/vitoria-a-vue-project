e46d19625164f5744cc69b421730cc1b38fdda47
vitoria-a <vitoriagomes@alu.ufc.br>
2022-05-04 12:07:22 -0300

    importing PrimeVue components

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Checkbox from 'primevue/checkbox';
 
 import 'primevue/resources/themes/saga-blue/theme.css';       //theme
 import 'primevue/resources/primevue.min.css';                 //core css
@@ -9,6 +12,9 @@ import 'primeicons/primeicons.css';                           //icons
 
 const app = createApp(App);
 app.use(PrimeVue);
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Checkbox', Checkbox);
 
 app.mount('#app');
 

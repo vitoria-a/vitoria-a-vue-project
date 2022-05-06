e5eb7782bfe194c39a5c0a2e32ed0cee6fa5c13d
vitoria-a <vitoriagomes@alu.ufc.br>
2022-05-06 17:32:53 -0300

    importing new PrimeVUe components

--- src/main.js
+++ src/main.js
@@ -9,11 +9,15 @@ import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Checkbox from 'primevue/checkbox';
+import ConfirmationService from 'primevue/confirmationservice';
+import ConfirmPopup from 'primevue/confirmpopup';
 
 const app = createApp(App);
 app.use(PrimeVue);
+app.use(ConfirmationService)
 app.component('InputText', InputText);
 app.component('Button', Button);
 app.component('Checkbox', Checkbox);
+app.component('ConfirmPopup', ConfirmPopup);
 app.mount('#app');
 

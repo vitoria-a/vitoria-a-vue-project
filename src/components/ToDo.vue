821b086693f7101148dc57e9aac4532695dfb6be
vitoria-a <vitoriagomes@alu.ufc.br>
2022-05-06 14:12:26 -0300

    changing the name of variables, methods, and adding new buttons

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="todo">
     <h3>Lista de tarefas</h3>
-    <InputText type="text" v-model="value" />
+    <InputText
+      placeholder="What do you need to do?"
+      type="text"
+      v-model="newTask"
+    />
 
-    <Button icon="pi pi-plus" iconPos="center" @click="toSave()" />
-    <Button class="p-button-danger" label="Clear List" @click="clearList()" />
+    <Button icon="pi pi-plus" iconPos="center" @click="save" />
+    <Button class="p-button-danger" label="Clear List" @click="clearToDo" />
 
-    <div class="task-list" v-for="(task, index) in tasks" :key="index">
-      <Checkbox :id="index" :binary="true" v-model="task.checked" />
-      <label class="label-task-name">{{ task.value }}</label>
+    <div :key="index" class="task-list" v-for="(task, index) in tasks">
+      <Checkbox :binary="true" :id="index" class="c" v-model="task.checked" />
+
+      <label
+        :class="[task.checked ? 'label-task-name-check' : 'label-task-name']"
+      >
+        {{ task.newTask }}
+      </label>
+      <Button icon="pi pi-pencil" @click="edit" />
+      <Button
+        class="p-button-danger"
+        icon="pi pi-times"
+        @click="clearTask(task)"
+      />
     </div>
   </div>
 </template>
@@ -17,46 +32,38 @@
 export default {
   name: "ToDo",
 
-  data: function () {
+  data() {
     return {
-      value: "",
+      newTask: "",
+      editTask: "",
       tasks: [],
-      checked: false,
     };
   },
 
   methods: {
-    toSave() {
-      this.tasks.push({
-        value: this.value,
-        checked: false,
-      });
-      this.value = "";
+    save() {
+      if (this.newTask !== "") {
+        this.tasks.push({
+          newTask: this.newTask,
+          checked: false,
+        });
+        this.newTask = "";
+      } else {
+        alert("Insira uma tarefa!");
+      }
     },
-    clearList() {
-      //Setting taskList to an empty array clears the whole list
+    clearToDo() {
       this.tasks = [];
+    },
+    clearTask(task) {
+      var index = this.tasks.indexOf(task);
+      this.tasks.splice(index, 1);
+    },
+    edit() {
+      this.tasks.put({
+        newTask: this.editTask,
+      });
     },
   },
 };
 </script>
-
-<style scoped>
-.task-list {
-  justify-content: start;
-  display: flex;
-  align-items: center;
-  margin-top: 10px;
-  width: 400px;
-  margin-left: 37%;
-}
-.p-button {
-  margin-left: 10px;
-}
-.label-task-name {
-  margin-left: 10px;
-}
-
-.p-checkbox {
-}
-</style>

b380c9112cd453837a47098bd702066eaef3fb52
vitoria-a <vitoriagomes@alu.ufc.br>
2022-05-06 13:57:09 -0300

    deleting comments in code

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import './assets/sass/styles.scss';
+
 import { createApp } from 'vue';
 import App from './App.vue';
-
 import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Checkbox from 'primevue/checkbox';
-
-import 'primevue/resources/themes/saga-blue/theme.css';       //theme
-import 'primevue/resources/primevue.min.css';                 //core css
-import 'primeicons/primeicons.css';                           //icons
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.component('InputText', InputText);
 app.component('Button', Button);
 app.component('Checkbox', Checkbox);
-
 app.mount('#app');
 
